9d44c4ec41437a427c4eec50815b9f0c84ad9b72
Mac Coombe <mac.coombe@csiro.au>
2019-05-22 15:48:08 +1000

    CPS-904: updated documentation, and implemented a few minor improvements.

--- tds_client/service/manager.py
+++ tds_client/service/manager.py
@@ -4,36 +4,32 @@ from collections.abc import Mapping
 
 from tds_client.service.ncss import NetCDFSubsetService
 from tds_client.service.opendap import OPeNDAPService
+from tds_client.util.strings import normalise
 
 
 class ServiceManager(Mapping):
     def __init__(self):
-        self._service_classes = set()
+        self._service_types = set()
         self._service_lookup = {}
 
     def __getitem__(self, key):
-        return self._service_lookup[key]
+        return self._service_lookup[normalise(key)]
 
     def __iter__(self):
-        return iter(self._service_lookup)
+        return iter(self._service_types)
 
     def __len__(self):
-        return len(self._service_lookup)
+        return len(self._service_types)
 
     def register_class(self, cls):
-        self._service_classes.add(cls)
+        self._service_types.add(cls.service_type)
 
-        self._service_lookup[cls.service_type] = cls
-        for alias in getattr(cls, 'aliases', []):
-            self._service_lookup[alias] = cls
+        for alias in cls.get_all_aliases():
+            self._service_lookup[normalise(alias)] = cls
 
     def load_plugins(self):
         for entry_point in iter_entry_points(group='tds_client.service'):
             self.register_service_class(entry_point.load())
-
-    @property
-    def classes(self):
-        return self._service_classes
 
 
 SERVICE_CLASSES = ServiceManager()
